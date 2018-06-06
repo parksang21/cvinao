23541b4370219dd23442ff7e5d38eac167b4ee0a
parksang21 <parksang1993@gmail.com>
2018-06-06 18:48:41 +0900

    Update custom.cpp

--- custom.cpp
+++ custom.cpp
@@ -1,26 +1,18 @@
-#pragma once
+#include "custom.h"
 
-#ifndef CL
-
-#define CL
-
-#include <iostream>
-#include <vector>
-#include <opencv2\opencv.hpp>
-
-#define PrintIVector(c, vector) {cout << endl << c << endl; for(int i : vector) cout << i << endl;}
-
-namespace cust
-{
-	template<typename T>
-	T getVectorAverage(std::vector<T> input) {
-		T sum = 0;
-		for (T iter : input) sum += iter;
-		return sum / (T)(input.size());
-	}
-
-	bool compareContourAreas(std::vector<cv::Point> contour1, std::vector<cv::Point> contour2);
-	double getAverageContourAreas(std::vector<std::vector<cv::Point>> conts, cv::Mat src);
+bool cust::compareContourAreas(std::vector<cv::Point> contour1, std::vector<cv::Point> contour2) {
+	double i = fabs(cv::contourArea(contour1));
+	double j = fabs(cv::contourArea(contour2));
+	return (i > j);
 }
 
-#endif
+double cust::getAverageContourAreas(std::vector<std::vector<cv::Point>> conts, cv::Mat src) {
+	double sum = 0.0;
+	std::vector<std::vector<cv::Point>>::iterator iter = conts.begin();;
+	while (iter != conts.end()) {
+		sum += contourArea(*iter);
+		iter++;
+	}
+	std::cout << sum;
+	return sum / (double)conts.size();
+}
