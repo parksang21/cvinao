9c192959373435c3ff89580d47e1c635fbb42b41
sang uk park <parksang1993@gmail.com>
2018-06-21 04:59:05 +0900

    laaasst commit

--- cviano/cviano/main.cpp
+++ cviano/cviano/main.cpp
@@ -78,34 +78,20 @@ int main() {
 
 	vc >> frame;
 
-	//cv::resize(frame, frame, cv::Size(1920, 1080));
-
 	//획득한 전체영상에서 흰부분 기준으로 건반의 위치에 사각형
 	detectKeyboard(frame, keyborad, keyboard_rect);
 
-
-	// to test keyboard detection
-	
-	imshow("SDAf", frame);
-	imshow("vv", keyborad);
-	cv::waitKey(0);
-	
-
-
-	//detectKeyboard2(frame);
-
 	//필요치는 않을것으로 예상되나, 사각형안의 건반에 대하여 삐뚤어졌을경우 똑바로 세워주는것
-	
 	//distortionRotation(frame,keyborad,keyboard_rect);
 
 	setWhiteKeyVector(frame, keyborad, keys, keyboard_rect);
 
-	//cv::imshow("frame", frame);
-	//cv::waitKey();
 	
 	//도레미파솔의 sens 값을 하드코딩 해보자
 	sihyun(keys, vc,preNote);
 	makeNote(preNote, output_note);
+
+	/*
 	std::cout << "====================pre===========================" << std::endl;
 	for (int i = 0; i < preNote.size(); i++) {
 		std::cout << "[" << preNote[i].first << ", " << preNote[i].second << "]" << std::endl;
@@ -115,6 +101,7 @@ int main() {
 	for (int i = 0; i < output_note.size(); i++) {
 		std::cout << "[" << output_note[i].first << ", " << output_note[i].second << "]" << std::endl;
 	}
-	
+	*/
+
 	return 0;
 }
